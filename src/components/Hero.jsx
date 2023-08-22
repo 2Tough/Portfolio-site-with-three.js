39831fe24a83b9b66a054c106ff401124bf0dd47
Polaski <vainferro@gmail.com>
2023-08-22 10:37:18 +0300

    Background added and working.
    Tried adding it before but didn't work. Now works.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import styled from "styled-components"
+
+const Section = styled.div`
+height: 100vh;
+
+scroll-snap-align: center;
+`
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <Section>Hero</Section>
     )
 }
 
